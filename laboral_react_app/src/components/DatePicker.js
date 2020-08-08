5152a659de564d7c673bb2ac9d8f1c9a1d2bc52a
Raul T <jr.torsa@gmail.com>
2020-08-08 00:41:22 -0600

    experimenting with date picker

--- laboral_react_app/src/components/DatePicker.js
+++ laboral_react_app/src/components/DatePicker.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
-import DatePicker, { registerLocale } from "react-datepicker";
+import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import es from "date-fns/locale/es";
+//import es from "date-fns/locale/es";
 
 const Dates = () => {
   const [startDate, SetStartDate] = useState(new Date("2000/01/01"));
   const [endDate, setEndDate] = useState(new Date());
+
+  console.log(startDate);
 
   return (
     <>
