8bd92b9cdc5fa6c38d85d3048f2cec1a3712d4b8
Raul T <jr.torsa@gmail.com>
2020-08-08 00:33:44 -0600

    added range date picker

--- laboral_react_app/src/components/DatePicker.js
+++ laboral_react_app/src/components/DatePicker.js
@@ -2,30 +2,29 @@ import React, { useState } from "react";
 import DatePicker, { registerLocale } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import es from "date-fns/locale/es";
-import { format } from "date-fns";
 
 const Dates = () => {
-  const [startDate, SetStartDate] = useState(new Date());
+  const [startDate, SetStartDate] = useState(new Date("2000/01/01"));
   const [endDate, setEndDate] = useState(new Date());
-
-  const onSelectDate = (date) => {
-    SetStartDate(date);
-    setEndDate(date);
-    console.log(date);
-  };
 
   return (
     <>
       <DatePicker
         startDate={startDate}
-        //selected={startDate}
-        onSelect={onSelectDate}
-        placeholderText="Ingreso"
+        selected={startDate}
+        onSelect={(date) => SetStartDate(date)}
+        selectsStart
+        endDate={endDate}
+        minDate={startDate}
+        showYearDropdown
       />
       <DatePicker
         endDate={endDate}
-        onSelect={onSelectDate}
-        placeholderText="Egreso"
+        onSelect={(date) => setEndDate(date)}
+        selected={endDate}
+        selectsEnd
+        startDate={startDate}
+        showYearDropdown
       />
     </>
   );
